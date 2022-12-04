c2958ace84f7e1530727ef61ae0a11fb0ae53f5f
onedeadream <schemerov8@mail.ru>
2022-12-04 15:12:36 +0300

    newproject

---
+++ src/test/java/com/clinic/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinic.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
